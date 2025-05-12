35ec831cc5b948f8f4d9f5960df39c82d2a3e70a
Eitan Schild <eitanisschild@gmail.com>
2025-05-12 19:28:24 +0300

    Update backend, ignore node_modules

--- index.js
+++ index.js
@@ -61,6 +61,12 @@ app.post('/auth', (req, res) => {
   res.sendStatus(401);
 });
 
+app.get('/test-session', (req, res) => {
+  console.log('🧪 TEST SESSION:', req.session);
+  res.json(req.session);
+});
+
+
 
 app.get('/admin.html', (req, res) => {
   console.log('🧠 SESSION CHECK (/admin.html):', req.session);
